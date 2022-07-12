d3ef203f7b49c099beb64352690772ddedd1832d
Luis Laporta <lui.laporta99@gmail.com>
2022-07-12 11:59:07 -0300

    Tests para a função fetchProducts criadas

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -5,4 +5,35 @@ const computadorSearch = require('../mocks/search');
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
   fail('Teste vazio');
+  it('Verifica se fetchProducts é uma function', () => {
+    expect(typeof(fetchProducts)).toEqual('function');
+  });
+  
+  it('Verifica se fetch é chamado', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Verifica se fetch utiliza o endpoint correto', async () => {
+    expect.assertions(1);
+    await fetchProducts('computador');
+
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  it('Verifica se o retorno da função fetchProducts é igual ao objeto computadorSearch', async () => {
+    expect.assertions(1);
+    const actual = await fetchProducts('computador');
+
+    expect(actual).toEqual(computadorSearch);
+  });
+
+  it('Verifica se retorna o erro "You must provide an url" ao chamar a função sem parametro', () => {
+    expect.assertions(1);
+    const actual = await fetchProducts();
+
+    expect(actual).toThrow('You must provide an url');
+  });
 });
